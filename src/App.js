8aea8e791aab5d90c6d1d0597ddf89c099ea6c49
pedroVflight <pedro@vflight.cl>
2021-04-06 02:01:32 -0400

    ✨ add navbar and containers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import './App.css';
 import './Card.css';
+import './Select.css';
+import React, { Component } from 'react';
+import Select from './Select';
 
-import Card from './Card';
 
-function App() {
-  return (
+
+
+class App extends Component {
+  render() {
+    return (
       <div>
-        <Card/>
+        <nav className="navbar navbar-light bg-light">
+          <div className="container-fluid">
+            <div className="navbar-brand mb-0 h1">
+              <span className="firs-word">BICE </span>
+              <span className="second-word">VIDA</span>
+            </div>
+          </div>
+        </nav>
+        <div className="container">
+          <br/>
+          <Select />
+          <br/>
+          <div id="insurances"></div>
+        </div>
       </div>
-  );
+    )
+  }
 }
 
 export default App;
