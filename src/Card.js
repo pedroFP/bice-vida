fe0fa94753b60cde737527f7b0e53d27eb267492
pedroVflight <pedro@vflight.cl>
2021-04-06 02:01:52 -0400

    ♻️ refactor card component

--- src/Card.js
+++ src/Card.js
@@ -1,31 +1,17 @@
 import React, { Component } from 'react';
 
 class Card extends Component {
-
-  state = {
-    name: '',
-    description: '',
-    price: '',
-    image: ''
-  };
-
-  async componentDidMount() { 
-    const url = 'https://dn8mlk7hdujby.cloudfront.net/interview/insurance/59';
-    const response = await fetch(url);
-    const data = await response.json();
-    this.setState(data.insurance);
-   }
   
   render() {
     return (
       <div className="card-base">
         <div className="card-img">
-          <label className="card-label">{this.state.price}</label>
-          <img src={this.state.image} alt="card-img"/>
+          <label className="card-label">{this.props.insurance.price}</label>
+          <img src={this.props.insurance.image} alt="card-img"/>
         </div>
         <div className="card-body">
-          <h1 className="card-title">{this.state.name} </h1>
-          <p className="card-text"> {this.state.description} </p>
+          <h1 className="card-title">{this.props.insurance.name} </h1>
+          <p className="card-text"> {this.props.insurance.description} </p>
         </div>
       </div>
     )
